fa8244d1e43f249b2e693f34aa06f0647000f1c5
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-14 22:21:53 -0500

    Currently logging to the client(browser console) and to the server(terminal)

--- chat_room/app.js
+++ chat_room/app.js
@@ -9,12 +9,23 @@ app.engine('jade', require('jade').__express);
 app.get('/', function (req, res) {
   res.render('index');
 });
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.jade');
+// });
 app.use(express.static(__dirname + '/public'))
 
 io.on('connection', function (client) {
   console.log('Client connected...');
 
+  // emit the messages event on the client
   client.emit('messages', { message: "Welcome to the chat!" });
+
+  client.on('send', function (data) {
+    client.broadcast.emit('messages', data);
+    // logs to the terminal console
+    console.log(data);
+  });
 });
 
 server.listen(8080);
--- chat_room/public/chat.js
+++ chat_room/public/chat.js
@@ -1,4 +1,18 @@
-var socket = io.connect('http://localhost:8080');
-socket.on('messages', function (data) {
-  alert(data.message);
+$(document).ready(function() {
+  var socket = io.connect('http://localhost:8080');
+
+  socket.on('messages', function (data) {
+    console.log(data.message);
+  });
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var text = $('#field').val();
+    socket.emit('send', text);
+    $('#field').val('');
+    // logs to the browser console
+    console.log(text);
+  });
 });
+
+
