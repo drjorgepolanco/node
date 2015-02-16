abf5049c4d8fadc6fd3953f847952c79227a69dc
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-16 05:57:13 -0500

    Create broadcast.js, send method and trim

--- pubsub/models/badges.js
+++ pubsub/models/badges.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var redis = require('../lib/redis');
+var broadcast = require('../lib/broadcast');
 
-// Save badges to database
-//  badges is an array
+// Save badges to database ('badges' is an array)
 exports.save = function(badges, callback) {
   if (!badges.length) return callback(null, null);
   var badge = badges.pop();
@@ -11,4 +11,18 @@ exports.save = function(badges, callback) {
     if (err) return callback(err, null);
     exports.save(badges, callback);
   });
+};
+
+// Trim down the redis list
+exports.trim = function() {
+  redis.ltrim('badges', 0, 9);
+};
+
+// Send out badges to the broadcaster
+exports.send = function(badges, callback) {
+  // badges.forEach(broadcast.send);  <----
+  badges.forEach(function(badge) { //      | same 
+    broadcast.send(badge);         //      | thing
+  });                              // <----
+  callback(null, null);
 };
