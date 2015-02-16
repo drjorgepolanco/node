8fac6f7bed269015ae9c340adf1c4c59df777149
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-02-16 07:28:14 -0500

    Add badges model, develop app.js, and create subscribe.js in lib

--- code_school_badges/webserver/app.js
+++ code_school_badges/webserver/app.js
@@ -5,6 +5,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var subSocket = require('./lib/subscribe');
+
 server.listen(3000, function() {
   console.log('Server is running on port %d', 3000);
 });
@@ -14,4 +16,17 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendfile('./public/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+  model.get(function(err, data) {
+    if (err) return;
+    data.forEach(function(badge){
+      socket.emit('badge', badge);
+    });
+  });
+});
+
+subSocket.on('message', function(message) {
+  io.sockets.emit('badge', message);
 });
